3e8c34e5d8dc438f94478e3f07765a1752934bc0
Rajkumarvt144 <rajkumarvt144@gmail.com>
2022-10-16 12:45:47 +0530

    coding modified

--- src/org/sample/Test.java
+++ src/org/sample/Test.java
@@ -7,6 +7,9 @@ public class Test {
 		System.out.println(3);
 		System.out.println(4);
 		System.out.println(5);
+		System.out.println(9);
+		System.out.println(10);
+		System.out.println(11);
 		
 	}
 }
